c08561198038998d2c4c895ab90a85b650ba0f8e
luisasarzi <luisa.cordeiro@escola.pr.gov.br>
2025-05-27 20:03:07 +0000

    Versão 2 – Dados dos elementos do jardim

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const buttons = document.querySelectorAll('#botoes button');
 const jardim = document.getElementById('Jardim');
 const mensagem = document.getElementById('mensagem');
-
+const plantData = {
+  Árvore: {
+    emoji: '🌳',
+    description: 'Árvore que purifica o ar e dá sombra para a cidade.',
+  },
+  Colheita: {
+    emoji: '🌾',
+    description: 'Colheita que alimenta a população da cidade.',
+  },
+  Rio: {
+    emoji: '💧',
+    description: 'Rio que fornece água limpa para todos.',
+  },
+  animais: {
+    emoji: '🐄',
+    description: 'Animal que ajuda na agricultura e alimentação.',
+  }
+};
